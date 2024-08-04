9b7c681457db453a2ecbd3467eea54ae081ff9f3
CHHuy <chehoanghuy2015@gmail.com>
2024-08-04 10:40:57 +0700

    Install Customize-CRA and setup babel-plugin-module-resolver

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* config-overrides.js */
+
+const {
+  override,
+  useBabelRc
+} = require("customize-cra");
+
+module.exports = override(
+  useBabelRc()
+);
