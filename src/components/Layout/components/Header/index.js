570022c5e028ae98da1734dbd2e261c386cc7235
CHHuy <chehoanghuy2015@gmail.com>
2024-10-03 21:48:46 +0700

    update UI Header & config component Button

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -3,6 +3,7 @@ import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Tippy from '@tippyjs/react/headless';
 
+import Button from '~/components/Button';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
@@ -12,11 +13,11 @@ const cx = classNames.bind(styles);
 
 function Header() {
     const [searchResult, setSearchResult] = useState([]);
-    useEffect(() => {
-        setTimeout(() => {
-            setSearchResult([]);
-        }, 0);
-    });
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setSearchResult([]);
+    //     }, 0);
+    // });
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
@@ -50,7 +51,10 @@ function Header() {
                         </button>
                     </div>
                 </Tippy>
-                <div className={cx('actions')}></div>
+                <div className={cx('actions')}>
+                    <Button text>Upload</Button>
+                    <Button primary>Log in</Button>
+                </div>
             </div>
         </header>
     );
